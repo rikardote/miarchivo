58f0fe4770679a060eb877ebe9f914c7283ad000
hector fuentes <ricarmenta@gmail.com>
2026-09-01 15:20:59 -0700

    feat(performance): enable Model::shouldBeStrict in non-production environments to prevent N+1 queries

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        \Illuminate\Database\Eloquent\Model::shouldBeStrict(! app()->isProduction());
+
         if (str_contains(config('app.url'), 'https://')) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
